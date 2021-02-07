8fedbb9aa8e37236b411d20da58045b4bc377bdc
smiles21 <scottso808@yahoo.com>
2021-02-06 19:36:44 -0600

    binary search implementation

---
+++ src/algorithms/binary-search/README.md
@@ -0,0 +1,13 @@
+# Binary Search
+
+Binary Search is a search algorithm which retutns the index of an element.
+Binary Search does not guarantee to return a specific index if multiple of the
+same element exist in the list.  There are extensions possible to allow for this
+case.
+
+## Usage
+
+### `binarySearch(list, value)`
+
+Returns the index of the value in the list, or -1 if the value is not present in
+the list
---
+++ src/algorithms/binary-search/recursive-binary-search.js
@@ -0,0 +1,21 @@
+const recursionHelper = (list, value, low, high) => {
+  if (low >= high && list[low] !== value) {
+    return -1;
+  }
+  
+  const midpoint = Math.floor((low + high) / 2);
+
+  if (list[midpoint] > value) {
+    return recursionHelper(list, value, low, midpoint - 1);
+  } else if (list[midpoint] < value) {
+    return recursionHelper(list, value, midpoint + 1, high);
+  } else {
+    return midpoint;
+  }
+};
+
+const recursiveBinarySearch = (list, value) => {
+  return recursionHelper(list, value, 0, list.length - 1);
+};
+
+module.exports = recursiveBinarySearch;
